d99fa2f14af435ac01f44e252c7cee906ca68647
Nicolas HODEE <nicolashodee@Nicolass-MacBook-Pro-2.local>
2022-04-14 12:24:37 +0200

    advancing on the homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import '../styles/NavBar.css'
+
+function NavBar() {
+
+  return (
+    <div className='agv-navbar'>
+      <h1>Aux Agrès du Vent</h1>
+      <ul>
+        <li>A propos</li>
+        <li>Cours</li>
+        <li>Stages</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+
+}
+
+export default NavBar
---
+++ src/components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Section() {
+  return (
+    <div className='homepage-section text-left'>
+
+      <div>
+        Partie de gauche avec image
+      </div>
+      <div>
+        Partie de droite avec texte
+      </div>
+    
+    </div>
+  )
+}
+
+export default Section
